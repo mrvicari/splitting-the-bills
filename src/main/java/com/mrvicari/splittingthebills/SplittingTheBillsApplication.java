57373167fe8e5e1dbda5ecdbdde6cb3a4c9baeea
Miguel Restrepo Vicari <miguelrestrepovicari@campus-10-41-1-158.wireless.leeds.ac.uk>
2018-01-26 15:33:59 +0000

    Initial structure

---
+++ src/main/java/com/mrvicari/splittingthebills/SplittingTheBillsApplication.java
@@ -0,0 +1,12 @@
+package com.mrvicari.splittingthebills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplittingTheBillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplittingTheBillsApplication.class, args);
+	}
+}
